54cbbd7947399a603e53ba5a067e602149ced84a
yasin_wolf <yasinkhoshbakht1400@gmail.com>
2024-01-22 12:13:39 +0330

    home page finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,16 @@ module.exports = {
     extend: {
       colors: {
         brand: "#FF9143",
+        card: "#606060",
+        testimonial: "#949494",
+        footer: "#131313",
         black2: {
           300: '#FFFFFF',
           500: '#E5E6EB',
           700: '#262A2C',
           900: '#1A1A1A',
         },
+        
       },
       fontFamily: {
         'Rubik': ['Rubik'],
